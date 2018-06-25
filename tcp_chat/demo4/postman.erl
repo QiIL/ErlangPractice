fdd0951f4487f9894b02f5cd5959ad9f8d72728c
QiIL <qsy_skx_code@qq.com>
2018-06-25 11:43:05 +0800

    tcp 聊天基础内容

--- tcp_chat/demo4/postman.erl
+++ tcp_chat/demo4/postman.erl
@@ -1,18 +1,30 @@
 -module(postman).
 -compile(export_all).
--export([post_feature/1]).
+-export([post_feature/2]).
 
 %% 这个模块的作用：1.如果需要添加Socket就把Socket添加进去告诉进程，
 %% 2.如果需要广播，就拿一堆socket然后逐个给发消息
-post_feature(SocketList) ->
+post_feature(SocketList, NickList) ->
     receive
-        {add, Socket} ->
-            post_feature([Socket|SocketList]);
-        {sub, Socket} ->
-            post_feature(lists:delete(Socket, SocketList));
         {broadcast, Socket, Msg} ->
+            io:format("~p~n", [SocketList]),
             boardcast(SocketList, Socket, Msg),
-            post_feature(SocketList)
+            post_feature(SocketList, NickList);
+        {online, Socket, Nick} ->
+            case lists:member(Nick, NickList) of
+                true -> 
+                    io:format("~p~n", [NickList]),
+                    gen_tcp:send(Socket, term_to_binary("the name: " ++ Nick ++ " has been use, goodbye!")),
+                    gen_tcp:close(Socket),
+                    post_feature(SocketList, NickList);
+                false ->
+                    boardcast(SocketList, Socket, string:concat(Nick, " is online!")),
+                    post_feature([Socket|SocketList], [Nick| NickList])
+            end;
+        {quit, Socket, Nick} ->
+            boardcast(SocketList, Socket, string:concat(Nick, " is quit~~")),
+            post_feature(lists:delete(Socket, SocketList), lists:delete(Nick, NickList)),
+            gen_tcp:close(Socket)
     end.
 
 boardcast([], _, _) ->
--- tcp_chat/demo4/server.erl
+++ tcp_chat/demo4/server.erl
@@ -5,13 +5,12 @@
 %%打开服务器，每一个连接都新建一个进程来循环等待Socket的消息
 start() ->
     {ok, Listen} = gen_tcp:listen(4000, [binary, {active, true}]),
-    Postman = spawn(postman, post_feature, [[]]),
+    Postman = spawn(postman, post_feature, [[], []]),
     pre_loop(Listen, Postman).
 
 %% 新建连接需要告诉其他进程增加了一个进程
 pre_loop(Listen, Postman) ->
     {ok, Socket} = gen_tcp:accept(Listen),
-    Postman ! {add, Socket}, %给postman发消息让postman去添加Socket
     spawn(fun() -> pre_loop(Listen, Postman) end),
     loop(Postman, Socket).
 
@@ -22,15 +21,14 @@ loop(Postman, Socket) ->
             io:format("Server (unpacked) ~p~n", [Str]),
             case Str of
                 {online, Nick} ->
-                    Postman ! {broadcast, Socket, Nick ++ "上线了"};
+                    Postman ! {online, Socket, Nick};
                 {talk, Nick, Msg} ->
                     Postman ! {broadcast, Socket, Nick ++ ":" ++ Msg};
                 {quit, Nick} -> 
-                    Postman ! {broadcast, Socket, Nick ++ "下线了"}
+                    Postman ! {quit, Socket, Nick}
             end,
             loop(Postman, Socket);
         {tcp_closed, _Socket} ->
-            Postman ! {sub, Socket},
             io:format("tcp server close!~n")
     end.
     
